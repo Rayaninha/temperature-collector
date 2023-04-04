ccadec844a3490fc83021858201a330ce6502b02
Rayaninha <salesrayane11@gmail.com>
2023-04-04 06:29:14 -0300

    :sparkles: feat: conexão WiFi

--- temperature-collector.ino
+++ temperature-collector.ino
@@ -1,27 +1,54 @@
-#include <Adafruit_Sensor.h>
+#include <WiFiEsp.h>
+#include <WiFiEspClient.h>
+#include <WiFiEspServer.h>
+#include <WiFiEspUdp.h>
 
-#include <DHT.h>
-#include <DHT_U.h>
-
-//Biblioteca Sensor DHT
+// #include <DHT.h>
+// #include <DHT_U.h>
 #include "DHT.h"
+
+#include "SoftwareSerial.h"
+
+SoftwareSerial esp8266(2, 3);
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
 
-//Declarar o sensor
+#define DEBUG true
+
+WiFiEspClient espClient;
+
+#define WIFI_AP "MUITILASER 1200AC 2.4G"
+#define WIFI_PASSWORD "123456789"
+
+int status = WL_IDLE_STATUS;
+unsigned long lastSend;
+
 DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
-  // put your setup code here, to run once:
 
   Serial.begin(9600);
-  Serial.println("Conectado a Porta::9600");
 
   dht.begin();
+
+  InitWiFi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  status = WiFi.status();
+  if(status != WL_CONNECTED) {
+    while( status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to WPA SSID: ");
+      Serial.println(WIFI_AP);
+
+      status = WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+      delay(500);
+    }
+    Serial.println("Connected to AP");
+  }
   getAndSendTemperatureAndHumidityData();
 }
 
@@ -52,5 +79,29 @@ void getAndSendTemperatureAndHumidityData() {
   Serial.print( "] -> " );
 
   delay(5000);
-
 }
+
+void InitWiFi() {
+    
+    esp8266.begin(19200);
+
+    WiFi.init(&esp8266);
+
+    if(WiFi.status() == WL_NO_SHIELD) {
+      Serial.println("WiFi shield not present");
+
+      while(true);
+    }
+
+    Serial.println("Connecting to AP ...");
+
+    while( status != WL_CONNECTED) {
+      Serial.print("Attemting to connect to WAP SSID: ");
+      Serial.println(WIFI_AP);
+
+      status = WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+      delay(500);
+    }
+    Serial.println("Connected to AP: ");
+    Serial.println(WiFi.localIP());
+  }
